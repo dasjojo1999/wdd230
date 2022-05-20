c55925a56d392ba0c9d9f8e6cfd4c764cd9383e8
dasjojo1999 <das21002@byui.edu>
2022-05-20 22:47:32 +0200

    commit

--- js/date.js
+++ js/date.js
@@ -1,2 +1,9 @@
-const options = {month: 'numeric', day: 'numeric', year: 'numeric'}
+const options = {year: 'numeric'}
 document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+
+const Update = document.lastModified
+document.querySelector("#update").textContent = Update;
+
+let currentdate = new Date();
+let Year = currentdate.getFullYear();
+document.getElementById("year").textContent = Year;
