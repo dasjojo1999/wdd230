1d9d69974642ad90ca2d8b2e8d6ca5d28683e807
dasjojo1999 <das21002@byui.edu>
2022-05-26 21:37:40 +0200

    commit

---
+++ chamber/js/date.js
@@ -0,0 +1,9 @@
+const options = {year: 'numeric'}
+document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+
+const Update = document.lastModified
+document.querySelector("#update").textContent = Update;
+
+let currentdate = new Date();
+let Year = currentdate.getFullYear();
+document.getElementById("year").textContent = Year;
