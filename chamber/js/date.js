ce77717d3dbfab9eb56a4b014c8afcad53610cd2
dasjojo1999 <das21002@byui.edu>
2022-05-28 23:06:02 +0200

    commit

--- chamber/js/date.js
+++ chamber/js/date.js
@@ -7,3 +7,21 @@ document.querySelector("#update").textContent = Update;
 let currentdate = new Date();
 let Year = currentdate.getFullYear();
 document.getElementById("year").textContent = Year;
+
+
+// select the elements to manipulate (output to)
+const datefield = document.querySelector(".current-date");
+const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+// long, medium, short options ... try them
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
