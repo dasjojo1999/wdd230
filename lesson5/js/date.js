392515291194c406ed4812e1093312829c25c10f
dasjojo1999 <das21002@byui.edu>
2022-06-28 22:10:35 +0200

    commit

--- lesson5/js/date.js
+++ lesson5/js/date.js
@@ -55,3 +55,24 @@ const close = document.querySelector("#close");
 close.addEventListener("click", () => {
     banner.style.display = "none";
 })
+
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+
+
+gridbutton.addEventListener("click", () => {
+	
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); 
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
+
