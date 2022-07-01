f00b4e86edfde13aa1adfc3a0fd511fb651b5a5f
dasjojo1999 <das21002@byui.edu>
2022-07-01 21:47:41 +0200

    commit

--- lesson5/js/date.js
+++ lesson5/js/date.js
@@ -1,4 +1,4 @@
-const options = {year: 'numeric'}
+const options = {weekday:'long',year: 'numeric', month:'long', day:'numeric'}
 document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
 
 const Update = document.lastModified
@@ -7,25 +7,6 @@ document.querySelector("#update").textContent = Update;
 let currentdate = new Date();
 let Year = currentdate.getFullYear();
 document.getElementById("year").textContent = Year;
-
-
-// select the elements to manipulate (output to)
-const datefield = document.querySelector(".current-date");
-const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
-
-// derive the current date using a date object
-const now = new Date();
-const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
-	now
-);
-const fulldateUK = new Intl.DateTimeFormat("en-UK", {
-	dateStyle: "full"
-}).format(now);
-// long, medium, short options ... try them
-
-datefield.innerHTML = `<em>${fulldate}</em>`;
-datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
-
 
 //Hamburger Menu
 function toggleMenu(){
